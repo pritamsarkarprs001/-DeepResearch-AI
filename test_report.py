bcc759454280129983522e74d8cccef1386aeffa
Pritam Sarkar <pritamsarkar.prs@gmail.com>
2026-07-02 01:23:16 +0600

    Delete test_report.py

--- test_report.py
+++
@@ -1,4 +0,0 @@
-from utils.report_generator import generate_pdf_report
-
-sample_md = "# Test Report\n\n## Introduction\nThis is a test.\n\n## Conclusion\nIt works!"
-generate_pdf_report(sample_md, "test_output.pdf")
